f4e375628cca398b5bf3e6f79ae32b8ec6a59e3b
TheoCou <theocourson@gmail.com>
2022-10-19 18:48:54 +0200

    modified:   src/header.inc.php

--- src/header.inc.php
+++ src/header.inc.php
@@ -1,14 +1,11 @@
 <header>
 
-
         <h1> <img class="shooting-star" src="./favicon/favicon-32x32.png" alt="shooting star"> Votre site de vente <b>PC Gamer</b></h1>
-
 
         <?php
 
         include_once './src/nav.inc.php';
         
         ?>
-
 
     </header>

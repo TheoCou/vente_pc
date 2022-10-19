a164014a46581f3a9a29f09cbade8f4598255e77
TheoCou <theocourson@gmail.com>
2022-10-19 17:26:13 +0200

    new file:   src/head.inc.php 	new file:   src/header.inc.php 	new file:   src/nav.inc.php

---
+++ src/header.inc.php
@@ -0,0 +1,14 @@
+<header>
+
+
+        <h1> <img class="shooting-star" src="./favicon/favicon-32x32.png" alt="shooting star"> Votre site de vente <b>PC Gamer</b></h1>
+
+
+        <?php
+
+        include_once './pages/nav.inc.php';
+        
+        ?>
+
+
+    </header>
---
+++ src/nav.inc.php
@@ -0,0 +1,15 @@
+<nav>
+
+            <a href="#">
+                <ul>
+
+
+                    <li class="list"> <span class="material-symbols-outlined">schedule</span> RETRAIT 1H</li>
+                    <li class="list"> <span class="material-symbols-outlined">local_shipping</span> LIVRAISON DÈS DEMAIN</li>
+                    <li class="list"> <span class="material-symbols-outlined">support_agent</span> SAV</li>
+
+
+                </ul>
+            </a>
+
+        </nav>

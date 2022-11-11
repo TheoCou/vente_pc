bbfc56c683442e6ed293aa8e8a3db317efe55ec5
TheoCou <theocourson@gmail.com>
2022-11-11 17:35:59 +0100

    modified:   index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once './src/head.inc.php';?>
+
+<body>
+
+<?php include_once './src/header.inc.php';?>
+
+<?php include_once './src/contenu.inc.php';?>
+
+<?php include_once './src/footer.inc.php';?>
+    
+</body>
+
